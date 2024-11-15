49a105638a0b141c9416851e700bb7a8125e3e0b
Shubhankar-24x <shubhankar2821@gmail.com>
2024-11-15 12:18:33 +0530

    Fixed issue

--- frontend/react-app/src/components/Login.jsx
+++ frontend/react-app/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {Link, useLocation, useNavigate } from 'react-router-dom';
 import {AuthContext} from '../contects/AuthProvider'
 import googleLogo from "../assets/google-logo.svg"
--- frontend/react-app/src/components/NavBar.jsx
+++ frontend/react-app/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBarsStaggered, FaBlog, FaXmark } from 'react-icons/fa6';
+import {AuthContext} from '../contects/AuthProvider';
 
 
 
